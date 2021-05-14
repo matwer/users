fadceb4f400b968b6b2236d6c1d5482092db35be
Matthew Werner <matwer@outlook.com>
2021-05-13 20:30:53 -0700

    UsersCRUD

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ app.secret_key = "keep it secret, keep it safe."
 def home():
     return redirect("/users")
 
+
 # CRUD
 
 # Read All
@@ -18,9 +19,25 @@ def index():
     
     query = "SELECT * FROM users;"
 
-    users = mysql.query_db(query)
+    user_list = mysql.query_db(query)
 
-    return render_template("index.html", all_users = users)
+    return render_template("index.html", all_users = user_list)
+
+
+# Read One
+@app.route("/users/<int:user_id>")
+def show_user(user_id):
+    mysql = connectToMySQL("users")
+
+    query = "SELECT * FROM users WHERE id = %(id)s;"
+
+    data = {
+        "id": user_id
+    }
+    
+    user_from_db = mysql.query_db(query, data)
+
+    return render_template("user.html", user = user_from_db[0])
 
 
 # Create
@@ -46,6 +63,52 @@ def add_user():
 
     return redirect("/users")
 
+
+# Update
+@app.route("/users/<int:user_id>/edit")
+def edit_user_form(user_id):
+    mysql = connectToMySQL("users")
+
+    query = "SELECT * FROM users WHERE id = %(id)s;"
+    
+    data = {
+        "id": user_id
+    }
+    
+    user_list = mysql.query_db(query, data)
+
+    return render_template("edit_user.html", user = user_list[0])
+
+@app.route('/users/<int:user_id>/update', methods = ['POST'])
+def update_user(user_id):
+    mysql = connectToMySQL("users")
+
+    query = "UPDATE users SET first_name = %(fname)s, last_name = %(lname)s, email = %(email)s, updated_at = NOW() WHERE id = %(id)s;"
+    data = {
+        "fname": request.form['first_name'],
+        "lname": request.form['last_name'],
+        "email": request.form['email'],
+        "id": user_id
+    }
+
+    mysql.query_db(query, data)
+
+    return redirect(f"/users/{user_id}")
+
+# Delete
+@app.route("/users/<int:user_id>/delete")
+def delete_user(user_id):
+    mysql = connectToMySQL("users")
+
+    query = "DELETE FROM users WHERE id = %(id)s;"
+
+    data = {
+        "id": user_id
+    }
+
+    mysql.query_db(query, data)
+
+    return redirect("/users")
 
 if __name__ == "__main__":
     app.run(debug = True)
